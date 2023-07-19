0e8aee09ae7a9e370efdab5f4ac4b3809b1344fe
Gopikrishnan G S <59282012+gopikrishnangs44@users.noreply.github.com>
2023-07-19 23:55:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pyMLR',
-    version='0.0.3',
+    version='0.0.4',
     author='Gopikrishnan G S',
     distribution='Python package for MLR',
     packages=['pyMLR'],
